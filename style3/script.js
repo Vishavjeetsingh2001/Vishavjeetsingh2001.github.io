afc745de8ff7e463dc94d308ec9478695b8a0879
Vishavjeetsingh2001 <90022915+Vishavjeetsingh2001@users.noreply.github.com>
2021-09-13 14:44:28 +0530

    Update script.js

--- style3/script.js
+++ style3/script.js
@@ -5,7 +5,7 @@ var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula"
 for (var i = 0; i < names.length; i++) {
   var firstLetter = names[i].charAt(0).toLowerCase();
 
-  if (firstLetter === 'j') {
+  if (firstLetter === 'J') {
     byeSpeaker(names[i]);
   }
   else {
